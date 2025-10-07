61d80d3cbb189693a5e67384905ce753fcfab52c
Doug Wegscheid <dwegscheid@sbcglobal.net>
2025-10-07 11:33:23 -0400

    Do transpose correctly. multiple songs can use the same input file.

--- typeset_setlist.py
+++ typeset_setlist.py
@@ -16,6 +16,39 @@ import weasyprint
 use_wkhtmltopdf = False
 
 
+def determine_key(key_index: int = None, accidentals: int = None):
+    if key_index < 0:
+        return "C"
+    key = ('C', 'D', 'E', 'F', 'G', 'A', 'B')[key_index]
+    key += 'b' if accidentals < 0 else ('#' if accidentals > 0 else '')
+    return key
+
+
+htfc = {
+    'C': 0,
+    'C#': 1,
+    'Db': 1,
+    'D': 2,
+    'D#': 3,
+    'Eb': 3,
+    'E': 4,
+    'F': 5,
+    'F#': 6,
+    'Gb': 6,
+    'G': 7,
+    'G#': 8,
+    'Ab': 8,
+    'A': 9,
+    'A#': 10,
+    'Bb': 10,
+    'B': 11
+}
+
+
+def determine_half_tones_from_c(key: str = None):
+    return htfc[key]
+
+
 def generate_path (tempdir_path, filename, suffix):
     prefix = pathlib.Path(filename).stem
     # _, rv = tempfile.mkstemp(dir=tempdir_path, suffix=suffix, prefix=prefix + '_')
@@ -23,8 +56,8 @@ def generate_path (tempdir_path, filename, suffix):
     return rv
 
 
-def generate_path_pdf(chordpro: str = None, transpose = 0, tempdir_path = None):
-    html_filename = generate_path(tempdir_path, chordpro, '.html')
+def generate_path_pdf(chordpro: str = None, transpose = 0, tempdir_path = None, index: int = 0):
+    html_filename = generate_path(tempdir_path, chordpro, f'_{index}.html')
 
     typeset_args = ['chordpro', '--generate=HTML', f'--output={html_filename}', '--diagrams=none', '--no-strict']
     if transpose is not None and transpose != 0:
@@ -36,7 +69,7 @@ def generate_path_pdf(chordpro: str = None, transpose = 0, tempdir_path = None):
         logging.error ('got rc = %d', typeset.returncode)
         return None
 
-    fixed_html_filename = generate_path(tempdir_path, chordpro, '_fixed.html')
+    fixed_html_filename = generate_path(tempdir_path, chordpro, f'_{index}_fixed.html')
     with open(html_filename) as fp:
         soup = BeautifulSoup(fp, 'html.parser')
 
@@ -50,7 +83,7 @@ def generate_path_pdf(chordpro: str = None, transpose = 0, tempdir_path = None):
         with open(fixed_html_filename, "wb") as file:
             file.write(soup.prettify("utf-8", formatter=None))
 
-    pdf_filename = generate_path(tempdir_path, chordpro, '.pdf')
+    pdf_filename = generate_path(tempdir_path, chordpro, f'_{index}.pdf')
 
     weasy = weasyprint.HTML(filename=fixed_html_filename)
     weasy.write_pdf(target=pdf_filename)
@@ -78,6 +111,9 @@ Setlists.Name,
 Songs.title,
 TextDisplaySettings.EnableTranpose,
 TextDisplaySettings.Transpose,
+TextDisplaySettings.Key,
+TextDisplaySettings.TransposeKey,
+TextDisplaySettings.UseSharps,
 Files.Path
 from Setlists
 left join SetlistSong on SetlistSong.SetListId = Setlists.Id
@@ -96,16 +132,35 @@ where Setlists.name = ?
     shutil.copy('chordpro.css', tempdir + '/chordpro_print.css')
 
     output_pdfs = [ ]
-    for f in res:
+    for i, f in enumerate(res):
+        logging.info ("got %s from database", f)
         #print(type(f), f)
 
         p = f['Path']
+        if p is None:
+            continue
         if not p.endswith('.chordpro'):
             continue
-        t = f['Transpose']
-        #print(p, t)
+        #et = f['EnableTranpose']  # they made a typo when they named it!
 
-        pdf_file = generate_path_pdf(chordpro=f'{args.dir}/{p}', transpose=t, tempdir_path=tempdir)
+        # per: https://www.zubersoft.com/mobilesheets/forum/thread-14026-post-59339.html#pid59339
+        #
+        # UseSharps is how many accidentals the transpose key has (-1 is flat, 0 is natural, 1 is sharp).
+        # Transpose is the letter from C to B where C is 0 and B is 6.
+        #
+        # TransposeKey is the number of accidentals for the document key.
+        # Key is the letter for the document key (like TransposeKey above, from C to B).
+        document_key = determine_key(f['Key'], f['TransposeKey'])
+        transpose_key = determine_key(f['Transpose'], f['UseSharps'])
+
+        document_halftones = determine_half_tones_from_c(document_key)
+        transpose_halftones = determine_half_tones_from_c(transpose_key)
+        transpose_amount = ((transpose_halftones - document_halftones) + 12) % 12
+        logging.info ('Transposing from %s (%s) to %s (%s)', document_key, document_halftones, transpose_key, transpose_halftones)
+        if transpose_key.endswith('b'):
+            transpose_amount = transpose_amount - 12
+
+        pdf_file = generate_path_pdf(chordpro=f'{args.dir}/{p}', transpose=transpose_amount, tempdir_path=tempdir, index=i)
         if pdf_file is not None:
             output_pdfs.append(pdf_file)
             #break
@@ -142,17 +197,18 @@ html, body {
         adding_page_count = adding_reader.get_num_pages()
         result_page_count = merger.get_num_pages()
 
-        logging.info ('adding %d pages from %s to existing %d pages',
-                      adding_page_count, pdf, result_page_count)
-
+        adding_message = ""
         if adding_page_count % 2 == 0:
             if result_page_count % 2 == 0:
-                logging.info('adding a blank page')
+                adding_message = 'adding a blank page, then '
                 merger.append(blank_page_pdf)
+
+        logging.info ('%sadding %d pages from %s to existing %d pages',
+                      adding_message, adding_page_count, pdf, result_page_count)
 
         merger.append(adding_reader)
 
-    merger.write("result.pdf")
+    merger.write(f'{args.dir}/{args.setlist}.pdf')
     merger.close()
 
 
